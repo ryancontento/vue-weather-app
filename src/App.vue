1569ab8fd1a117aced43bac288e9d1bca92f3a21
Ryan Contento <contento.rk@gmail.com>
2022-01-03 21:05:09 -0500

    style mostly finished

--- src/App.vue
+++ src/App.vue
@@ -106,6 +106,29 @@ main {
 
   .weather-box {
     text-align: center;
+  }
+
+  .weather-box .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: #FFF;
+    font-size: 102px;
+    font-weight: 900;
+
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 16px;
+    margin: 30px 0px;
+
+  box-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  }
+
+  .weather-box .weather {
+    color: #FFF;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 
   }
 </style>
